ff3eb7c502d88ac96e97526dd63852d04c98aac8
Kaique <makaiquee@gmail.com>
2023-03-06 19:18:25 -0300

    CRUD completo

---
+++ DB/PizzaDB.cs
@@ -0,0 +1,54 @@
+﻿namespace Pizzaria.DB
+{
+    public record Pizza
+    {
+        public int Id { get; set; }
+        public string ? Nome { get; set; }
+    }
+
+    public class PizzaDB
+    {
+        private static List<Pizza> _pizzas = new List<Pizza>()
+        {
+            new Pizza{ Id=1, Nome="Mussarela" },
+            new Pizza{ Id=2, Nome="Calabresa" },
+            new Pizza{ Id=3, Nome="Marguerita" },
+            new Pizza{ Id=4, Nome="Portuguesa" }
+        };
+
+        public static List<Pizza> GetPizzas()
+        {
+            return _pizzas;
+        }
+
+        public static Pizza ? GetPizza(int id)
+        {
+            return _pizzas.SingleOrDefault(pizza => pizza.Id == id);
+        }
+
+        public static Pizza CreatePizza(Pizza pizza)
+        {
+            _pizzas.Add(pizza);
+            return pizza;
+        }
+
+        public static Pizza UpdatePizza(Pizza update) 
+        {
+            _pizzas = _pizzas.Select(pizza =>
+            {
+                if (pizza.Id == update.Id)
+                {
+                    pizza.Nome = update.Nome;
+                }
+                return pizza;
+
+            }).ToList();
+            return update;
+        }
+
+        public static void RemovePizza(int id)
+        {
+            _pizzas = _pizzas.FindAll(pizza => pizza.Id != id).ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.OpenApi.Models;
+using Pizzaria.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizzaria API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizzaria API v1");
+});
+
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+app.Run();
